53d2ea694eb6d1ecd485b5b9c8a034e84eff3704
Courtenay <court@Courtenays-MacBook-Pro.local>
2018-12-14 17:25:46 -0700

    connect database

--- server/server.js
+++ server/server.js
@@ -5,15 +5,15 @@ const massive = require('massive')
 const { decks } = require('./decks')
 const {
     SERVER_PORT,
-    CONNECTION_STRING,
+    // CONNECTION_STRING,
 } = process.env
 
 const app = express()
 
-massive(CONNECTION_STRING).then(db => {
-    console.log('db connected')
-    app.set('db', db)
-})
+// massive(CONNECTION_STRING).then(db => {
+//     console.log('db connected')
+//     app.set('db', db)
+// })
 
 app.get('/flachecards', (req, res) => {
     res.send(decks)
